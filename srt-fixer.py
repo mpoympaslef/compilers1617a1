c1d2fda4b73eb06165f2a014c9c942498e2f46f7
mpoympaslef <teris93@hotmail.gr>
2017-05-09 23:04:09 +0300

    Update srt-fixer.py

--- srt-fixer.py
+++ srt-fixer.py
@@ -1,9 +1,8 @@
-
 import sys
 import re
 import argparse
 
-
+		
 parser = argparse.ArgumentParser()
 # add mandatory (positional) arguments
 parser.add_argument("fname",help="input srt file name")
@@ -12,12 +11,40 @@ parser.add_argument("offset",type=float,help="subtitle offset in seconds to appl
 # parse arguments
 args = parser.parse_args()
 
+secs = args.offset
+
 with open(args.fname,newline='') as ifp:	
 	for line in ifp:
-	
-		# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
-
-		sys.stdout.write(line)
-
-		# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
-
+		
+		res = re.search('-->',line)
+		
+		if res == None:
+			# -- αντικαταστήστε με τον δικό σας κώδικα (αρχή) --
+			sys.stdout.write(line)
+		else:
+			# -- αντικαταστήστε με τον δικό σας κώδικα (τέλος) --
+			
+			times = re.split('-->',line)
+			import datetime
+			
+			str_date1=times[0].strip()
+			str_date2=times[1].strip()
+			
+			fmt='%H:%M:%S,%f'
+			date1 = datetime.datetime.strptime(str_date1, fmt)
+			date2 = datetime.datetime.strptime(str_date2, fmt)
+			
+			
+			date1 = date1 + datetime.timedelta(0,secs)
+			date2 = date2 + datetime.timedelta(0,secs)
+			
+			str_date1 = str(date1.time())
+			str_date1 = str_date1.replace('.',',')
+			str_date1 =str_date1[0:len(str_date1)-3]
+			
+			str_date2 = str(date2.time())
+			str_date2 = str_date2.replace('.',',')
+			str_date2 =str_date2[0:len(str_date2)-3]
+			
+			line = str_date1+' --> '+str_date2+'\n'
+			sys.stdout.write(line)
